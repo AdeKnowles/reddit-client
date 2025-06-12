7cca9d548b028ec9752c20843e38c7be8ee9bc02
AdeKnowles <158853032+AdeKnowles@users.noreply.github.com>
2025-06-12 06:18:03 +0100

    Updated vite.config.js because of build error

--- reddit-client/vite.config.js
+++ reddit-client/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build:  { target: 'esnext' },  // production build
+  esbuild:{ target: 'esnext' },  // dev server
+  // base: '/my-react-app/',     // ← keep or add this if you publish to a repo site
+});
